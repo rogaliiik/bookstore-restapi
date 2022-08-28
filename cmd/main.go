f07ef4cde3cdf3eaf980c8ef4aa1390225641263
Artyom Galkin <igalart2000@gmail.com>
2022-08-28 23:39:54 +0300

    models_db_config_utils

---
+++ cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"github.com/rogaliiik/bookstore/pkg/routes"
+)
+
+func main() {
+	router := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(router)
+
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
+
+}
